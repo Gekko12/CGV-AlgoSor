c2c13f29a677ab370bbf03ca945bb9605e792dda
Gekko12 <gauravsharma01042000@gmail.com>
2021-08-05 15:53:46 +0530

    almost completed

--- algosor.cpp
+++ algosor.cpp
@@ -4,18 +4,20 @@
 
 using namespace std;
 
-#define SORT_NO 2 // Number of sorting algorithms
-#define MAX_SIZE 30		// Number of values in the array
-#define SPEED 500	// High val means slow
+#define SORT_NO 2		// Number of sorting algorithms
+#define MAX_SIZE 30 // Number of values in the array
+#define SPEED 2000	// High val means slow
 
-int a[MAX_SIZE];				// Array
+int a[MAX_SIZE];
 int swapflag = 0; // Flag to check if swapping has occured
-int i = 0, j = 0; // To iterate through the array
-int flag = 0;			// For Insertion Sort
-int k = 0;				// To Switch from Welcome screen to Main Screen
-int sorting = 0;	// 1 if Sorted
+int i = 0, j = 0;
+int flag = 0;
+int k = 0;			 // To Switch from Welcome screen to Main Screen
+int sorting = 0; // 1 if Sorted
 const char *sort_string[] = {"Insertion Sort", "Bubble Sort"};
 int sort_count = 0; // To cycle through the string
+int esc_key = 0;
+unsigned int speed_strength = 5; //sorting_speed = SPEED/speed_strength ....
 
 // Function to display text on screen char by char
 void bitmap_output(int x, int y, const char *string, void *font)
@@ -26,7 +28,6 @@ void bitmap_output(int x, int y, const char *string, void *font)
 	len = (int)strlen(string);
 	for (i = 0; i < len; i++)
 	{
-		// glScalef(0.005, 0.005, 1);
 		glutBitmapCharacter(font, string[i]);
 	}
 }
@@ -94,38 +95,42 @@ void int_str(int rad, char r[])
 
 void display_text()
 {
-	glColor3f(0, 1, 0);
+	glColor3f(0, 0, 1);
 	bitmap_output(170, 750, "SIMULATION OF INSERTION AND BUBBLE SORT ALGORITHMS", GLUT_BITMAP_TIMES_ROMAN_24);
 	glEnd();
 
-	/*
-	bitmap_output(10, 710, "Made by:", GLUT_BITMAP_9_BY_15);
-	glColor3f(1, 1, 0);
-	bitmap_output(10, 690, "Gaurav Sharma ", GLUT_BITMAP_9_BY_15);
-	glColor3f(1, 1, 1);
-	bitmap_output(150, 690, "( 1AT18CS28 )", GLUT_BITMAP_9_BY_15);
-	glColor3f(1, 1, 0);
-	bitmap_output(10, 670, "Sumukha Hegde ", GLUT_BITMAP_9_BY_15);
-	glColor3f(1, 1, 1);
-	bitmap_output(90, 670, "( 1AT18CS129 )", GLUT_BITMAP_9_BY_15);
-	*/
+	glColor3f(1, 0.2, 1);
+	glBegin(GL_POLYGON);
+	glVertex2i(150, 740);
+	glVertex2i(560, 740);
+	glVertex2i(560, 735);
+	glVertex2i(150, 735);
+	glEnd();
+
 	if (sorting == 0) // if not sorting display menu
 	{
 		glColor3f(1, 0, 0);
 		bitmap_output(10, 700, "MENU", GLUT_BITMAP_9_BY_15);
-		glColor3f(0, 1, 0);
+		glColor3f(0, 0, 0);
 		bitmap_output(10, 670, "Press s to SORT", GLUT_BITMAP_9_BY_15);
 		bitmap_output(10, 650, "Press c to SELECT the sort algorithm", GLUT_BITMAP_9_BY_15);
 		bitmap_output(10, 630, "Press r to RANDOMISE", GLUT_BITMAP_9_BY_15);
 		bitmap_output(10, 610, "Esc to QUIT", GLUT_BITMAP_9_BY_15);
 		bitmap_output(10, 590, "Selected sort: ", GLUT_BITMAP_9_BY_15);
-		glColor3f(1, 1, 0);
+		glColor3f(1, 0, 1);
 		bitmap_output(80, 590, *(sort_string + sort_count), GLUT_BITMAP_9_BY_15);
+		glColor3f(0, 0, 0);
+		bitmap_output(10, 570, "Speed l(low) and h(high): ", GLUT_BITMAP_9_BY_15);
+		glColor3f(1, 0, 1);
+		char text[10];
+		int_str(speed_strength, text);
+		bitmap_output(130, 570, text, GLUT_BITMAP_9_BY_15);
 	}
 	else if (sorting == 1) // while sorting
 	{
+		glColor3f(0, 0, 0);
 		bitmap_output(10, 690, "Selected sort: ", GLUT_BITMAP_9_BY_15);
-		glColor3f(1, 1, 0);
+		glColor3f(1, 0, 1);
 		bitmap_output(80, 690, *(sort_string + sort_count), GLUT_BITMAP_9_BY_15);
 		glColor3f(0.5, 0.5, 0.5);
 		bitmap_output(10, 650, "Sorting in progress...", GLUT_BITMAP_9_BY_15);
@@ -136,28 +141,48 @@ void display_text()
 
 void front()
 {
-	glColor3f(0, 1, 0);
-	bitmap_output(318, 565, "WELCOME", GLUT_BITMAP_TIMES_ROMAN_24);
+	glColor3f(0, 0, 1);
+	bitmap_output(318, 470, "WELCOME", GLUT_BITMAP_TIMES_ROMAN_24);
 	glEnd();
-	bitmap_output(340, 525, "TO", GLUT_BITMAP_TIMES_ROMAN_24);
+	bitmap_output(340, 430, "TO", GLUT_BITMAP_TIMES_ROMAN_24);
 	glEnd();
-	bitmap_output(165, 475, "SIMULATION OF INSERTION AND BUBBLE SORT ALGORITHMS", GLUT_BITMAP_TIMES_ROMAN_24);
+	bitmap_output(165, 390, "SIMULATION OF INSERTION AND BUBBLE SORT ALGORITHMS", GLUT_BITMAP_TIMES_ROMAN_24);
 	glEnd();
 
-	/*
-	glColor3f(1, 0, 0);
-	bitmap_output(300, 350, "Made by:", GLUT_BITMAP_TIMES_ROMAN_24);
-	glColor3f(1, 1, 0);
-	bitmap_output(190, 310, "Gaurav Sharma ", GLUT_BITMAP_TIMES_ROMAN_24);
-	glColor3f(1, 1, 1);
-	bitmap_output(390, 310, "( 1AT18CS128 )", GLUT_BITMAP_TIMES_ROMAN_24);
-	glColor3f(1, 1, 0);
-	bitmap_output(240, 270, "Sumukha Hegde ", GLUT_BITMAP_TIMES_ROMAN_24);
-	glColor3f(1, 1, 1);
-	bitmap_output(345, 270, "( 1AT18CS129 )", GLUT_BITMAP_TIMES_ROMAN_24);
-	*/
 	glColor3f(1, 0, 1);
 	bitmap_output(550, 17, "Press Enter to continue.......", GLUT_BITMAP_HELVETICA_18);
+}
+
+void Contributors()
+{
+	glColor3f(1, 0.2, 1);
+	glBegin(GL_POLYGON);
+	glVertex2i(200, 540);
+	glVertex2i(500, 540);
+	glVertex2i(500, 580);
+	glVertex2i(200, 580);
+	glEnd();
+
+	glColor3f(0, 0, 0.5);
+	bitmap_output(320, 470, "Made by -", GLUT_BITMAP_TIMES_ROMAN_24);
+	glEnd();
+
+	glColor3f(0, 0, 1);
+	bitmap_output(280, 430, "Gaurav Sharma (1AT18CS128)", GLUT_BITMAP_HELVETICA_18);
+	glEnd();
+
+	bitmap_output(280, 390, "Sumukha Hegde (1AT18CS129)", GLUT_BITMAP_HELVETICA_18);
+	glEnd();
+
+	glColor3f(1, 0.2, 1);
+	glBegin(GL_POLYGON);
+	glVertex2i(200, 320);
+	glVertex2i(500, 320);
+	glVertex2i(500, 280);
+	glVertex2i(200, 280);
+	glEnd();
+
+	glutTimerFunc(SPEED * 2, exit, 0);
 }
 
 void Initialize()
@@ -168,13 +193,14 @@ void Initialize()
 	for (temp1 = 0; temp1 < MAX_SIZE; temp1++)
 	{
 		a[temp1] = rand() % 100 + 1;
-		printf("%d ", a[temp1]);
+		// printf("%d ", a[temp1]);
 	}
 
 	// Reset all values
 	i = j = 0;
 	flag = 0;
 
+	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0, 699, 0, 799);
@@ -200,7 +226,7 @@ void display()
 
 	if (k == 0)
 		front();
-	else
+	else if (k == 1 && !esc_key)
 	{
 		display_text();
 		char text[10];
@@ -226,22 +252,25 @@ void display()
 			glLineWidth(1);
 
 			int_str(a[ix], text);
-			glColor3f(0, 1, 0);
-			bitmap_output(12 + (700 / (MAX_SIZE + 1)) * ix, 35, text, GLUT_BITMAP_TIMES_ROMAN_10);
+			glColor3f(1, 0, 1);
+			bitmap_output(12 + (700 / (MAX_SIZE + 1)) * ix, 30, text, GLUT_BITMAP_HELVETICA_12);
 		}
 
 		if (swapflag || sorting == 0)
 		{
-			glColor3f(0, 1, 0);
+			glColor3f(1, 1, 0);
 			glBegin(GL_POLYGON);
 			glVertex2f(10 + (700 / (MAX_SIZE + 1)) * j, 50);
 			glVertex2f(10 + (700 / (MAX_SIZE + 1)) * (j + 1), 50);
-			glVertex2f(10 + (700 / (MAX_SIZE + 1)) * (j + 1), 50 + a[j] * 4);
-			glVertex2f(10 + (700 / (MAX_SIZE + 1)) * j, 50 + a[j] * 4);
+			glVertex2f(10 + (700 / (MAX_SIZE + 1)) * (j + 1), 50 + a[j] * 5);
+			glVertex2f(10 + (700 / (MAX_SIZE + 1)) * j, 50 + a[j] * 5);
 			glEnd();
 			swapflag = 0;
 		}
 	}
+	else
+		Contributors();
+
 	glFlush();
 }
 
@@ -273,7 +302,7 @@ void insertionsort()
 			{
 				flag = 0;
 			}
-			printf("swap %d and %d\n", a[j], a[j + 1]);
+			// printf("swap %d and %d\n", a[j], a[j + 1]);
 		}
 		i++;
 	}
@@ -302,7 +331,7 @@ void bubblesort()
 			j++;
 			if (j == MAX_SIZE - 1)
 				j = 0;
-			printf("swap %d and %d\n", a[j], a[j + 1]);
+			// printf("swap %d and %d\n", a[j], a[j + 1]);
 		}
 	}
 	sorting = 0;
@@ -325,10 +354,9 @@ void makedelay(int)
 		}
 	}
 	glutPostRedisplay();
-	glutTimerFunc(SPEED , makedelay, 0);
+	glutTimerFunc(SPEED / speed_strength, makedelay, 0);
 }
 
-// Keyboard Function
 void keyboard(unsigned char key, int x, int y)
 {
 	if (key == 13)
@@ -338,7 +366,10 @@ void keyboard(unsigned char key, int x, int y)
 		switch (key)
 		{
 		case 27:
-			exit(0); // 27 is the ascii code for the ESC key
+			esc_key = 1;
+			Contributors();
+			break;
+			// exit(0); // 27 is the ascii code for the ESC key
 		case 's':
 			sorting = 1;
 			break;
@@ -347,6 +378,13 @@ void keyboard(unsigned char key, int x, int y)
 			break;
 		case 'c':
 			sort_count = (sort_count + 1) % SORT_NO;
+			break;
+		case 'h':
+			speed_strength++;
+			break;
+		case 'l':
+			if (speed_strength > 1)
+				speed_strength--;
 			break;
 		}
 	}
